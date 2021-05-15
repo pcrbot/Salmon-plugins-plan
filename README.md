f71885bd8964e33d189730e0599f1fd14df1a859
WatanabeAsa <watanabe9940@gmail.com>
2021-05-15 22:20:21 +0800

    5/15

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 一个适用于 [Salmonbot](https://github.com/Watanabe-Asa/SalmonBot) 的插件新建及迁移企画。
 
+欢迎大家参与本项目。
+
 ## 使用方法
 
 将所需插件文件夹置于路径`salmon/modules`下，并按照对应要求安装依赖或编辑配置，然后在文件`salmon/configs/__bot__.py`的`MODULES_ON`中添加插件名并重启 bot .
@@ -15,6 +17,21 @@
 原项目导航> [picfinder](https://github.com/pcrbot/picfinder_take)
 
 请将文件`picfinder.template.py`移动至路径`salmon/configs`下并重命名为`picfinder.py`，并按注释编辑配置。
+
+</details>
+
+<details>
+  <summary>check</summary>
+
+### 搜图功能
+
+原项目导航> [check](https://github.com/pcrbot/Hoshino-plugin-transplant/tree/master/check)
+
+请将文件`check.template.py`移动至路径`salmon/configs`下并重命名为`check.py`，并按注释编辑配置。然后安装依赖。
+
+```python
+pip3.9 install psutil
+```
 
 </details>
 
---
+++ check/self_check.py
@@ -0,0 +1,30 @@
+import salmon
+from salmon import Bot, Service, priv, scheduler
+from salmon.typing import CQEvent
+from salmon.modules.check.check import Check
+
+
+sv = Service('check', use_priv=priv.SUPERUSER, manage_priv=priv.SUPERUSER, visible=False)
+
+MAX_PERFORMANCE_PERCENT = salmon.configs.check.MAX_PERFORMANCE_PERCENT
+PROCESS_NAME_LIST = salmon.configs.check.PROCESS_NAME_LIST
+check = Check(salmon.configs.check.PROCESS_NAME_LIST)
+
+check_self = sv.on_fullmatch('check', aliases={'自检', '自檢', '自我检查', '自我檢查'}, only_group=False)
+
+@check_self.handle()
+async def _(bot: Bot, event: CQEvent):
+    check_report_admin = await check.get_check_info()
+    if check_report_admin:
+        await bot.send(event, check_report_admin)
+    else:
+        salmon.logger.error("Not found Check Report")
+        await bot.send(event, "[ERROR]Not found Check Report")
+
+
+@scheduler.scheduled_job('cron', id='自我检查', hour='*/3', minute='13')
+async def check_task():
+    weihu = salmon.configs.SUPERUSERS[0]
+    result = await check.get_check_easy()
+    for bot in salmon.get_bot_list():
+        await bot.send_private_msg(user_id=weihu, message=result)
